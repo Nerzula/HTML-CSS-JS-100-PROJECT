86b800c2f76146f4761ad70c96578c84a9de8916
Alex <alexzloid@gmail.com>
2024-01-22 01:08:26 +0500

    Cursor Project Added

---
+++ 17.cursor/app.js
@@ -0,0 +1,10 @@
+const cursor = document.querySelector('.cursor');
+
+const moveCursor = function (pageX, pageY) {
+	cursor.style.left = pageX + 'px';
+	cursor.style.top = pageY + 'px';
+};
+
+document.addEventListener('mousemove', function (e) {
+	moveCursor(e.pageX, e.pageY);
+});
